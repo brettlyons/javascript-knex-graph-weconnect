b9e84d4bece3ed3febad0c78e567bdf2338d84cd
Brett Lyons <blyons@students.naropa.edu>
2015-12-28 11:11:08 -0700

    First commit

---
+++ seeds/create_users.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('users').del(), 
+
+    // Inserts seed entries
+    knex('users').insert({name:'Alfred'}),
+    knex('users').insert({name:'Barnaby'}),
+    knex('users').insert({name:'Colleen'}),
+    knex('users').insert({name:'Darby'}),
+    knex('users').insert({name:'Eleanor'}),
+    knex('users').insert({name:'Fernando'})
+  );
+};
