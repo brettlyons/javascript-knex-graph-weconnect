6550ba328798ab5be76308ced45647d7b8905eb0
Brett Lyons <blyons@students.naropa.edu>
2015-12-29 17:10:18 -0700

    Updated some things.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const knex = require('knex')(require('../knexfile')['development']);
-
+const adjacencyList = require('../lib/adjacencyList').adjacencyList;
+const findConnections = require('../lib/adjacencyList').findConnections;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('users').then(function (users) {
@@ -13,16 +14,13 @@ router.get('/users/:id', function(req, res, next) {
   knex('users').where({id: req.params.id}).first().then(function(user) {
 
     knex('connections')
-      .innerJoin('users', 'other_id', 'users.id')
-      .where({user_id: user.id})
+      .select('user_id', 'users.name', 'other_id', 'others.name as other_name')
+      .innerJoin('users', 'user_id', 'users.id')
+      .innerJoin('users AS others', 'other_id', 'others.id')
       .then(function (connections) {
-
-        knex('connections')
-          .where({other_id: user.id})
-          .innerJoin('users', 'user_id', 'users.id')
-          .then(function (connections2) {
-            res.render('user', { user: user, connections: connections.concat(connections2) });
-          });
+        var myList = adjacencyList(connections);
+        var results = findConnections(myList, req.params.id);
+        res.render('user', {user:user, list: myList, results:results});
       });
   });
 });
